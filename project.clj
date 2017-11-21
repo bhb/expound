92ee04efd882142bfd0c0392dd82eab287ae9936
Ben Brinckerhoff <ben@bbrinck.com>
2017-11-20 17:16:55 -0700

    Try rc1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/bhb/expound"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta3" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1" :scope "provided"]
                  [org.clojure/clojurescript "1.9.946" :scope "provided"]
                  [org.clojure/spec.alpha "0.1.143" :scope "provided"]]
 
