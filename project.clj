fff5e387e489e0b43ddc878d7d6e03fedb1d242e
Ben Brinckerhoff <ben@bbrinck.com>
2018-02-06 21:01:49 -0700

    v0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject expound "0.4.1-SNAPSHOT"
+(defproject expound "0.5.0"
   :description "Human-optimized error messages for clojure.spec"
   :url "https://github.com/bhb/expound"
   :license {:name "Eclipse Public License"
