1d05dcdc14cc9e53123cc51264e3f06ec90d04fe
Ben Brinckerhoff <ben@bbrinck.com>
2018-10-24 18:02:29 -0600

    Fix tests

--- bin/golden_master_test.sh
+++ bin/golden_master_test.sh
@@ -7,4 +7,4 @@ mydir=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXXXXXXXXX")
 pushd bin && clojure sample.clj > "$mydir/output.txt"
 popd
 
-diff -u "$mydir/output.txt" ./test/expected_sample_out.txt
+diff -u ./test/expected_sample_out.txt "$mydir/output.txt"
