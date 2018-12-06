1a35e7d5f7d9de8c3d990419114af95af5a57825
Ben Brinckerhoff <ben@bbrinck.com>
2018-12-05 21:09:39 -0700

    Test clojure 1.9 with older version of spec

--- project.clj
+++ project.clj
@@ -115,9 +115,11 @@
                                         [karma-reporter "3.1.0"]]}]
              :cljs-repl {:dependencies [[com.cemerick/piggieback "0.2.2"]]}
              :clj-1.9.0-alpha19 {:dependencies [[org.clojure/clojure "1.9.0-alpha19"]]}
-             :clj-1.9.0 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             :clj-1.9.0 {:dependencies [[org.clojure/clojure "1.9.0"]
+                                        ]}
              :cljs-1.10.238 {:dependencies  [[org.clojure/clojurescript "1.10.238"]]}
-             :cljs-1.10.339 {:dependencies [[org.clojure/clojurescript "1.10.339"]]}}
+             :cljs-1.10.339 {:dependencies [[org.clojure/clojurescript "1.10.339"]]}
+             :spec-0.2.168  {:dependencies [[org.clojure/spec.alpha "0.2.168"]]}}
   :aliases {"run-tests-once" ["with-profile" "test-web" "cljsbuild" "once" "test"]
             "run-tests-auto" ["do"
                               ["with-profile" "test-web" "cljsbuild" "once" "test"]
