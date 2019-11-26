7b2552dfa0ba0f400e10e84a1d451d3d35bb17ea
Ben Brinckerhoff <ben@bbrinck.com>
2019-11-25 17:44:31 -0700

    Add step to catch accidental dependencies

---
+++ bin/deps_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mydir=$(mktemp -d "${TMPDIR:-/tmp/}$(basename $0).XXXXXXXXXXXX")
+
+lein install
+pushd bin && clojure deps.clj > "$mydir/actual_deps.txt"
+popd
+
+diff -u <(cat ./test/expected_deps.txt | perl -pe 's/__\d+\#//g') \
+        <(cat "$mydir/actual_deps.txt" | perl -pe 's/__\d+\#//g')
